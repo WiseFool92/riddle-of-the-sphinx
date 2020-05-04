32f1892ff10526f8acbcd5258d46965abdf2e7e5
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-04 13:53:21 -0700

    finish random question functionality

--- riddle.cs
+++ riddle.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 
 class Riddle
 {
-  static int count = 1;
+  // static int count = 1;
+  static int[] asked = { 0, 0, 0 };
   static void Main()
   {
     Console.WriteLine("Welcome to the desert. I am the Sphinx. Answer my 3 riddles to access my treasure. Guess wrong and perish");
@@ -18,15 +19,21 @@ class Riddle
     Dictionary<int, string> riddleDictionary = new Dictionary<int, string>() { { 1, "What 4-letter word can be written forward, backward or upside down, and can still be read from left to right?" },
     {2, "What can be caught but never thrown?"}, {3, "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?" } };
 
+    Random rand = new Random();
+    int count = rand.Next(1, 4);
 
-    if (count == 1)
+
+    if (count == 1 && asked[0] == 0)
     {
       Console.WriteLine(riddleDictionary[count]);
       string userAnswer = Console.ReadLine();
       string answer = RiddleAnswer(count);
+      asked[0] = count;
+      Console.WriteLine(count);
+      Console.WriteLine(asked[0]);
       if (userAnswer == answer)
       {
-        count += 1;
+        // count += 1;
         AskRiddle();
       }
       else
@@ -34,14 +41,17 @@ class Riddle
         Console.WriteLine("Incorrect. You died");
       }
     }
-    else if (count == 2)
+    else if (count == 2 && asked[1] == 0)
     {
       Console.WriteLine(riddleDictionary[count]);
       string userAnswer = Console.ReadLine();
       string answer = RiddleAnswer(count);
+      asked[1] = count;
+      Console.WriteLine(count);
+      Console.WriteLine(asked[1]);
       if (userAnswer == answer)
       {
-        count += 1;
+
         AskRiddle();
       }
       else
@@ -49,19 +59,30 @@ class Riddle
         Console.WriteLine("Incorrect. You died");
       }
     }
-    else if (count == 3)
+    else if (count == 3 && asked[2] == 0)
     {
       Console.WriteLine(riddleDictionary[count]);
       string userAnswer = Console.ReadLine();
       string answer = RiddleAnswer(count);
+      asked[2] = count;
+      Console.WriteLine(count);
+      Console.WriteLine(asked[2]);
       if (userAnswer == answer)
       {
-        Console.WriteLine("Congratulations, my treaure of yours.");
+        AskRiddle();
       }
       else
       {
         Console.WriteLine("You've made it far but incorrect. You died");
       }
+    }
+    else if (asked[0] != 0 && asked[1] != 0 && asked[2] != 0)
+    {
+      Console.WriteLine("Here's my treasure");
+    }
+    else
+    {
+      AskRiddle();
     }
   }
 
@@ -75,4 +96,5 @@ class Riddle
     return (ans);
 
   }
+
 }
